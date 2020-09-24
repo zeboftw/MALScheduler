30c8ffbc4a1d50755421c546d89c92ea19eeca4e
Zebo <malex.zebo.work@gmail.com>
2020-09-24 23:41:40 +0300

    Messed around with Qt library to plan out how the application will look.

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 # This is a sample Python script.
 from urllib.request import urlopen
 import json
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(100, 100, 1000, 1000)
+    win.setWindowTitle("Test run")
 
+    label = QtWidgets.QLabel(win)
+    label.setText("My first label")
+    label.move(50, 50)
+
+    win.show()
+    sys.exit(app.exec_())
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
@@ -15,4 +29,5 @@ if __name__ == '__main__':
     data = json.load(json_obj)
     for anime in data['anime']:
         print(anime['title'])
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    window()
